13f3a5050cfa3ad42906435d09067542e7605fab
Charbel t <charbel2000.52@gmail.com>
2020-12-05 10:49:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function saturdayFun(task='roller-skate'){
-  return 'This Saturday, I want to '+task;
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
 }
 
-function mondayWork(activity="go to the office"){
-  return "This Monday, I will "+activity;
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
 }
 
 let wrapAdjective = function(style="*") {
   return function(adjective="special") {
-    return 'You are '+style+' '+adjective+' '+style
+    return `You are ${style}${adjective}${style}!`
   }
 }
 
